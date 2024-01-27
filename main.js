846dad84c22f2fb843ccfe9309b7924c6071ccc6
Prince Singh <princesingh808087@gmail.com>
2024-01-27 22:32:50 +0530

    completed the orbit controls

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
@@ -13,7 +14,7 @@ pointLight.position.set(10, 0, 3);
 scene.add(pointLight);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById("app").appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(5, 5, 5);
 //const geometry = new THREE.CapsuleGeometry(1, 1, 10, 20);
@@ -28,19 +29,24 @@ const sphereMaterial = new THREE.MeshBasicMaterial({ color: "white" });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
+sphere.position.set(20, 0, 10);
+pointLight.position.set(20, 0, 10);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 let q = 0;
 function animate() {
+	controls.update();
 	q += 0.01;
 	let qSin = Math.sin(q);
 	let qCos = Math.cos(q);
-	cube.position.x = 3 * qSin;
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	cube.rotation.z += 0.01;
-	let scaledCos = 30 * qCos;
-	let scaledSin = 30 * qSin;
-	sphere.position.set(scaledCos, 0, scaledSin);
-	pointLight.position.set(scaledCos, 0, scaledSin);
+	// cube.position.x = 3 * qSin;
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+	// cube.rotation.z += 0.01;
+	// let scaledCos = 30 * qCos;
+	// let scaledSin = 30 * qSin;
+	// sphere.position.set(scaledCos, 0, scaledSin);
+	// pointLight.position.set(scaledCos, 0, scaledSin);
 	renderer.render(scene, camera);
 
 	requestAnimationFrame(animate);
